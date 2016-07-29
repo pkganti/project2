f9415f8826d38f644b577c391bde3808f36e31f8
Katrina Lee <katrinamarielee@gmail.com>
2016-07-29 15:05:10 +1000

    Moved colon in models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-  belongs_to: user
-  has_many: ingredients :through => quantities
-  has_many: quantities
+  belongs_to :user
+  has_many :ingredients :through => :quantities
+  has_many :quantities
 end
