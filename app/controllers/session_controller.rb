91fe45bc1e8f1244870fd43181026535b14c7303
Phani Ganti <phaniganti@Phanis-MacBook-Pro.local>
2016-07-29 15:28:09 +1000

    Added the session and has secure password and fetch user under Applications controller and user rb and home html

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,18 @@
+class SessionController < ApplicationController
+
+  def create
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
