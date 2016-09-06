4aa9fe29a9a6036c206adab0308e3223a69c7f76
Tripti <Tripti@Triptis-MacBook-Air.local>
2016-09-06 10:12:21 +1000

    added functionality to include api search results in favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -23,31 +23,68 @@ class FavoritesController < ApplicationController
 
   def add
     f1 = Favorite.new
-    #  binding.pry
     user = @current_user
-    id = params[:id]
-    recipe = Recipe.find(id)
-    # raise 'help'
+    binding.pry
+    if JSON.parse(params[:recipe][:api_recipe])['source_url'].nil?
+      id = params[:id]
+      recipe = Recipe.find(id)
+      added_to_fav = Favorite.where(:user_id => user.id , :recipe_id => id)
+      #neded to add to fav via regular
+      if (added_to_fav.empty?)
+        user.favorites << f1
+        recipe.favorites << f1
 
-    added_to_fav = Favorite.where(:user_id => user.id , :recipe_id => id)
-    added_by_me = Recipe.where(:user_id => user.id, :id => id)
+        f1.save
+        if (f1.save)
+          #flash[:notice] = "Successfully bookmarked your recipe !!"
+          success = true
+        end
 
-    if (added_to_fav.empty? && added_by_me.empty?)
-      user.favorites << f1
-      recipe.favorites << f1
-
-      f1.save
-      if (f1.save)
-        #flash[:notice] = "Successfully bookmarked your recipe !!"
-        success = true
+      else
+      #   flash[:notice] = "Already bookmarked !!"
+        success = false
       end
 
+      respond_to do |format|
+        format.json  { render :json => { :status => success } }
+        format.html  {redirect_to(recipe_path(recipe))}
+      end
     else
-    #   flash[:notice] = "Already bookmarked !!"
-      success = false
+      #create the recipe and then add to fav
+      # recipe_api = Recipe.create api_recipe_params
+      recipe_params = JSON.parse(params[:recipe][:api_recipe])
+      recipe_api = Recipe.new
+      recipe_api.title = recipe_params['title']
+      recipe_api.images = recipe_params['images']
+      recipe_api.ratings = recipe_params['ratings']
+      recipe_api.prep_duration = recipe_params['prep_duration']
+      recipe_api.cook_duration = recipe_params['cook_duration']
+      recipe_api.servings = recipe_params['servings']
+      recipe_api.directions = recipe_params['directions']
+      # recipe_api.ingredients = params['recipe']['api_recipe']['ingredients']
+      recipe_api.source_url = recipe_params['source_url']
+      recipe_api.user_id =  @current_user.id
+      # binding.pry
+      if recipe_params['ingredients'].present?
+        recipe_params['ingredients'].each do |i|
+           ing = Ingredient.new
+           ing.name = i
+           ing.save
+           recipe_api.ingredients << ing
+        end
+      end
+      recipe_api.save
+
+      user.favorites << f1
+      recipe_api.favorites << f1
+
+      # redirect_to :back
+      respond_to do |format|
+        format.json  { render :json => recipe_api, :status => success }
+        format.html  {redirect_to(recipe_path(recipe_api))}
+      end
+      # render :json => recipe_api ,  :status => success
     end
-    render :json => { :status => success }
-    # redirect_to :back
 
   end
 
